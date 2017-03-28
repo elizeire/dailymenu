127565e33c54d33a41656a5760111a46ce8b7e97
Guilherme Elizeire <guilherme.elizeire@cz.ibm.com>
2017-03-28 12:47:50 +0200

    dailymenu first commit

---
+++ src/main/java/com/brno/DailymenuApplication.java
@@ -0,0 +1,12 @@
+package com.brno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailymenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailymenuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brno/DailymenuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brno;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DailymenuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
